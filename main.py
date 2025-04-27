0fc5315db9c281a2457f05d840a8919c2cdeeb01
Aakash Yadav <Aakashsyadav1999@gmail.com>
2025-04-27 12:12:04 +0530

    added snowflake uploader

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from src.pipeline.inference_pipeline import InitiateInference
 
 if __name__ == "__main__":
     inti_pipeline = InitiateInference()
-    raw_data = inti_pipeline.fetching_data()
-    print(f"This is the raw data {raw_data}")
+    inti_pipeline.fetching_data()
+    transformed_data = inti_pipeline.transform_data()
+    print(f"This is the transformed data {len(transformed_data)}")
+    inti_pipeline.load_data_to_snowflake(transformed_data)
