ace270f718636bdf6870fe7dade5496ffedb858a
Aakash Yadav <Aakashsyadav1999@gmail.com>
2025-04-27 12:14:51 +0530

    Added snowflake database connector

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,5 +1,9 @@
 import os
 from datetime import datetime
+from dotenv import load_dotenv, find_dotenv
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
 
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
@@ -10,7 +14,17 @@ LOGS_FILE_NAME = "SIDFC.log"
 
 # Data Ingestion 
 ROOT_DIR= "DataFetchArtifacts"
-SOURCE_URL= "https://drive.google.com/file/d/1qrlF01WOC210RfPm3jdSnU6sG_hew7aM/view?usp=sharing"
+SOURCE_URL= os.getenv('SOURCE_URL')
 UNZIP_DIR= ROOT_DIR
-LOCAL_FILE_PATH = "sample_submission.zip"
+LOCAL_FILE_PATH = "raw_file.zip"
 UNZIP_DIR_CSV_DATA = "DataTransformationArtifacts"
+
+# Snowflake connection details
+# Snowflake connection parameters
+SNOWFLAKE_ACCOUNT = os.getenv('SNOWFLAKE_ACCOUNT')
+SNOWFLAKE_USER = os.getenv('SNOWFLAKE_USER')
+SNOWFLAKE_PASSWORD = os.getenv('SNOWFLAKE_PASSWORD')
+SNOWFLAKE_WAREHOUSE = os.getenv('SNOWFLAKE_WAREHOUSE')
+SNOWFLAKE_DATABASE = os.getenv('SNOWFLAKE_DATABASE')
+SNOWFLAKE_SCHEMA = os.getenv('SNOWFLAKE_SCHEMA')
+SNOWFLAKE_TABLE = os.getenv('SNOWFLAKE_TABLE')
